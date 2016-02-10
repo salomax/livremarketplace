f23011ca12f93c4c50b4792bc54e5621fd6a7a82
salomax <salomao.marcos@gmail.com>
2016-02-10 15:34:31 -0200

    Purchase fetures, pages and put service

--- static/app/purchase/purchase.js
+++ static/app/purchase/purchase.js
@@ -3,34 +3,23 @@ var API_ROOT = '//' + document.location.host + '/_ah/api';
 purchase = {
 
 	bindList : function(_data) {
-		// Construir tabela com as compras
-		$('table.table-purchases').bootstrapTable({
-			columns : [{
-			        field: 'product',
-			        title: 'Produto'
-			    }, {
-			        field: 'quantity',
-			        title: 'Quantidade'
-			    }, {
-			        field: 'cost',
-			        title: 'Custo'
-			    }, {
-			        field: 'supplier',
-			        title: 'Fornecedor'
-			    }],	
-			data : _data.items	
-		});
 
-		$('table.table-purchases').DataTable({
-          "paging": true,
-          "lengthChange": false,
-          "searching": true,
-          "ordering": false,
-          "info": false,
-          "autoWidth": false
-        });
+		alert (_data.items.length);
 
-        $('div.fixed-table-loading').remove();
+		// Caso haja algum dado a tabela é montada		
+		if (_data.items.length > 0) {
+
+			$('table.table-purchases').DataTable({
+	          "paging": true,
+	          "lengthChange": false,
+	          "searching": true,
+	          "ordering": false,
+	          "info": false,
+	          "autoWidth": false
+	        });
+
+		}
+
 
 	},
 
@@ -43,8 +32,52 @@ purchase = {
 				purchase.bindList(response);
 			});
 		}, API_ROOT);
-	}	
+	},
+
+	put : function() {
+
+		gapi.client.load('purchase', 'v1', function() {
+
+			// Serialize form
+			var form = $('form.purchase-form');
+			console.log(JSON.stringify($('form').serializeObject()));
+
+ 			var data = JSON.stringify($('form').serializeObject());
+
+			var request = gapi.client.purchase.put($('form').serializeObject());
+			request.execute(function(response) {
+				// Atachar a lista de compras na tabela
+				// purchase.bindList(response);
+
+				console.log(response);
+			});
+
+		}, API_ROOT);
+
+		return false;
+	}
+
 };
 
+// Carregar a lista de compras asinc
 purchase.load();
 
+// Atachar o formulário ao endpoint
+$('form.purchase-form').bind('submit', purchase.put);
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || null);
+        } else {
+            o[this.name] = this.value || null;
+        }
+    });
+    return o;
+};
