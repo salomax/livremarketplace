96ca9221caa40a9f825579129183f65b745135c2
salomax <salomao.marcos@gmail.com>
2016-02-25 07:19:08 -0300

    Code style pep8

--- app/dashboard/messages.py
+++ app/dashboard/messages.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding: utf-8
+# coding: utf-8
 #
 # Copyright 2016, Marcos Salomão.
 #
@@ -14,44 +14,50 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+from protorpc import messages
+from protorpc import message_types
+
 __author__ = "Marcos Salomão"
 __email__ = "salomao.marcos@gmail.com"
 __copyright__ = "Copyright 2016, Marcos Salomão"
 __license__ = "Apache 2.0"
 
 
-from protorpc import messages
-from protorpc import message_types
-
 class IntGetMessage(messages.Message):
-  	"""Mensagem GET a ser trafegada pelo endpoint representando um valor do tipo ponto flutuante."""
+    """Mensagem GET a ser trafegada pelo endpoint
+       representando um valor do tipo ponto flutuante.
+    """
 
-	# Valor 
-	value = messages.IntegerField(1, required=True)
+    # Valor
+    value = messages.IntegerField(1, required=True)
 
 
 class FloatGetMessage(messages.Message):
-  	"""Mensagem GET a ser trafegada pelo endpoint representando um valor do tipo ponto flutuante."""
+    """Mensagem GET a ser trafegada pelo endpoint
+       representando um valor do tipo ponto flutuante.
+    """
 
-	# Valor 
-	value = messages.FloatField(1, required=True)
+    # Valor
+    value = messages.FloatField(1, required=True)
 
 
 class CashFlowGetMessage(messages.Message):
-  	"""Mensagem GET a ser trafegada pelo endpoint representando o fluxo de caixa"""
+    """Mensagem GET a ser trafegada pelo endpoint
+       representando o fluxo de caixa
+    """
 
-	# período
-	period = message_types.DateTimeField(1, required=True)
+    # período
+    period = message_types.DateTimeField(1, required=True)
 
-	# compras
-	purchases = messages.FloatField(2, required=False)
+    # compras
+    purchases = messages.FloatField(2, required=False)
 
-	# vendas
-	sales = messages.FloatField(3, required=False)
+    # vendas
+    sales = messages.FloatField(3, required=False)
 
 
 class CashFlowCollectionMessage(messages.Message):
-	"""Coleção de fluxo de caixa."""
+    """Coleção de fluxo de caixa."""
 
-	items = messages.MessageField(CashFlowGetMessage, 1, repeated=True)
+    items = messages.MessageField(CashFlowGetMessage, 1, repeated=True)
